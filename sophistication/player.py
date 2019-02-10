32093488d17f2998a214733582a3f2ed6ab02c1c
Adoria298 <bananaorange29@outlook.com>
2019-02-10 17:01:06 +0000

    Prevented the player from bouncing

--- sophistication/player.py
+++ sophistication/player.py
@@ -17,19 +17,25 @@ class Player(arcade.Sprite):
     def update(self):
         """
         Moves this class by self.change_x and self.change_y.
-        Attempts to prevent players being outside the visible window.
+        Attempts to prevent players from being outside the visible window
+        (bouncing).
         """
-        if (self.center_x + self.change_x) >= SCREEN_WIDTH:
-            self.change_x = 0
-        if (self.center_y + self.change_y) >= SCREEN_HEIGHT:
-            self.change_y = 0 
-        if (self.center_x + self.change_x)<= -SCREEN_WIDTH:
-            self.change_x = 0
-        if (self.center_y + self.change_y) <= -SCREEN_HEIGHT:
-            self.change_y = 0
-
+        
         self.center_x += self.change_x
         self.center_y += self.change_y    
+
+        # prevents x-axis bouncing
+        if self.left < 0:
+            self.left = 0
+        if self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+        
+        # prevents y-axis bouncing
+        if self.bottom < 0:
+            self.bottom = 0
+        if self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+        
         self.change_x = 0
         self.change_y = 0
 
