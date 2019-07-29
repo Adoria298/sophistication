a4926af60d3c15a63062b4cdcda12cffa8ee8535
Adoria298 <bananaorange29@outlook.com>
2019-07-29 16:54:09 +0100

    Made traders die properly.

--- src/trader.py
+++ src/trader.py
@@ -37,12 +37,12 @@ class Trader(arcade.Sprite):
         # the bearing between two points can be found from atan2(x2-x1, y2-y1)
         # https://stackoverflow.com/questions/3932502/calculate-angle-between-two-latitude-longitude-points
         # added some randomness for effect
-        END_ACCURACY = 20
+        END_ACCURACY = 10
         b = math.atan2(self.end[0]-self.center_x, self.end[1]-self.center_y)
         #self.angle += (b + random.randint(0,360)) % 360 # modulo'd so it's <360
         if ((self.center_x != self.end[0] and self.center_y != self.end[1])
-            or (self.center_x <= self.end[0]-END_ACCURACY and self.center_x <= self.end[0]+END_ACCURACY)
-            or (self.center_y <= self.end[1]-END_ACCURACY and self.center_y <= self.end[1]+END_ACCURACY)):
+            and (self.center_x <= self.end[0]-END_ACCURACY and self.center_x <= self.end[0]+END_ACCURACY)
+            and (self.center_y <= self.end[1]-END_ACCURACY and self.center_y <= self.end[1]+END_ACCURACY)):
             self.center_x += self.speed * b
             self.center_y += self.speed * b
         else:
